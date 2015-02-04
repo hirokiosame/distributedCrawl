5f9dcbdf31ad15d30eb3385f70b65981cd4ac2e5
Hiroki Osame <hirokio@bu.edu>
2015-02-03 21:22:32 -0500

    made it clearer to use

--- index.js
+++ index.js
@@ -1,107 +1,128 @@
-module.exports = function(host, username, password, database, table){
+(function(){
 
+	var enqueue = function(sequelize){
 
-	var sequelize = require("./sequelize")(host, database, username, password, "crawl1");
+		var insertQueue = [], timeout;
 
-	var clusterDistribution = require("clusterDistribution");
+		return function enqueue(rows){
 
+			// IF array
+			if( rows instanceof Array ){ return rows.forEach(enqueue); }
 
-	var insertQueue = [];
-	function enqueue(rows){
+			// Ignore if not string
+			if( typeof rows !== "string" ){ return false; }
 
-		// IF array
-		if( rows instanceof Array ){ return rows.forEach(enqueue); }
+			clearTimeout(timeout);
 
-		// Ignore if not string
-		if( typeof rows !== "string" ){ return false; }
+			insertQueue.push(rows);
 
+			timeout = setTimeout(function(){
 
-		insertQueue.push(rows);
+				var spliced = insertQueue.splice(0, insertQueue.length);
+				if( spliced.length === 0 ){ return; }
 
-		setTimeout(function(){
+				sequelize.Schemas.then(function(){
 
-			var spliced = insertQueue.splice(0, insertQueue.length);
-			if( spliced.length === 0 ){ return; }
+					sequelize.Schemas.queue.bulkCreate(
+						spliced.map(function(row){
+							return {
+								url: row,
+								parentId: null,
+								level: 1
+							};
+						}),
+						{
+							ignoreDuplicates: true
+						}
+					)
+					.complete(function(err, rows){
+						if( err ){ throw new Error(err); }
+						// console.log("Bulk created", arguments);
+					});
 
-			sequelize.Schemas.then(function(){
-
-				sequelize.Schemas.queue.bulkCreate(
-					spliced.map(function(row){
-						return {
-							url: row,
-							parentId: null,
-							level: 1
-						};
-					}),
-					{
-						ignoreDuplicates: true
-					}
-				)
-				.complete(function(err, rows){
-					if( err ){ throw new Error(err); }
-					// console.log("Bulk created", arguments);
 				});
 
-			});
+			}, 100);
+		};
+	};
 
-		}, 100);
-	}
 
-	function startCrawling(options){
-		return function(){
+	function main(credentials, options, importer){
+
+		// Validate credentials
+		if( typeof credentials !== "object" || !(credentials instanceof Object) ){ throw new Error("Credentials is an invalid object"); }
+
+		if( !["host", "user", "password", "database", "table"].every(function(prop){
+			return	credentials.hasOwnProperty(prop) &&
+					typeof credentials[prop] === "string" &&
+					credentials[prop].length > 0;
+		}) ){ throw new Error("Credentials don't have valid properties"); }
+
+		// Validate options
+		if( typeof options !== "object" || !(credentials instanceof Object) ){
+			throw new Error("Options is an invalid object");
+		}
+
+		// Validate importer
+		if( typeof importer !== "function" ){ throw new Error("Importer must be a function"); }
+
+
+		// Connect to DB
+		var sequelize = require("./sequelize")(credentials.host, credentials.database, credentials.user, credentials.password, credentials.table);
+
+		var clusterDistribution = require("clusterDistribution");
+
+		// Start import
+		importer(enqueue(sequelize), function startCrawling(){
 			clusterDistribution(
 				options.workers,
 				require("./master")(sequelize),
 				require("./worker")
 			);
-		};
-	}
-
-	return function(options, importer){
-
-		// Start import
-		importer(enqueue, startCrawling(options));
+		});
 	};
-};
 
-if( require.main === module ){
+	if( require.main === module ){
 
-	var read = require("read");
+		var read = require("read");
 
-	function check( prompt, value, callback ){
-		if( value === undefined ){
-			read({ prompt: prompt+": " }, function(err, answer){
-				if( err ){ return callback(err); }
-				else{ return callback(null, answer); }
-			});
-		}else{
-			// Return
-			console.log(prompt+":", value);
-			callback(null, value);
+		function check( prompt, value, callback ){
+			if( value === undefined ){
+				read({ prompt: prompt+": " }, function(err, answer){
+					if( err ){ return callback(err); }
+					else{ return callback(null, answer); }
+				});
+			}else{
+				// Return
+				console.log(prompt+":", value);
+				callback(null, value);
+			}
 		}
+
+		// Get commandline arguments
+		var args = process.argv.slice(2);
+
+		// Else, prompt user
+		check("Host", args[0], function(err, host){
+
+		if( err ){ throw err; }
+		check("Username", args[1], function(err, username){
+
+		if( err ){ throw err; }
+		check("Password", args[2], function(err, password){
+
+		if( err ){ throw err; }
+		check("Database", args[3], function(err, database){
+
+		if( err ){ throw err; }
+		check("Table", args[4], function(err, table){
+
+		if( err ){ throw err; }
+
+			main(host, username, password, database, table);
+
+		}); }); }); }); });
+	}else{
+		module.exports = main;
 	}
-
-	// Get commandline arguments
-	var args = process.argv.slice(2);
-
-	// Else, prompt user
-	check("Host", args[0], function(err, host){
-
-	if( err ){ throw err; }
-	check("Username", args[1], function(err, username){
-
-	if( err ){ throw err; }
-	check("Password", args[2], function(err, password){
-
-	if( err ){ throw err; }
-	check("Database", args[3], function(err, database){
-
-	if( err ){ throw err; }
-	check("Table", args[4], function(err, table){
-
-	if( err ){ throw err; }
-
-		module.exports(host, username, password, database, table);
-
-	}); }); }); }); });
-}
+})();
